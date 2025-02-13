9629e5f9df6281bb44d3b6ff27eeb286ee5a672e
DarkMode224 <hiworldover@gmail.com>
2025-02-13 09:08:04 -0800

    fromat_prettier

--- server/middlewares/helpers/multer.js
+++ server/middlewares/helpers/multer.js
@@ -1,66 +1,71 @@
-const path = require("path");
-const multer = require("multer");
+const path = require('path');
+const multer = require('multer');
 
 //user's..
 const storageByUser = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/uploads");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname +
-                "-" +
-                req.user._id +
-                "-" +
-                Date.now() +
-                path.extname(file.originalname)
-        );
-    },
+          destination: function (req, file, cb) {
+                    cb(null, './public/uploads');
+          },
+          filename: function (req, file, cb) {
+                    cb(
+                              null,
+                              file.fieldname +
+                                        '-' +
+                                        req.user._id +
+                                        '-' +
+                                        Date.now() +
+                                        path.extname(file.originalname)
+                    );
+          },
 });
 
 //admin's..
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/uploads");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname +
-                "-" +
-                req.profile._id +
-                "-" +
-                Date.now() +
-                path.extname(file.originalname)
-        );
-    },
+          destination: function (req, file, cb) {
+                    cb(null, './public/uploads');
+          },
+          filename: function (req, file, cb) {
+                    cb(
+                              null,
+                              file.fieldname +
+                                        '-' +
+                                        req.profile._id +
+                                        '-' +
+                                        Date.now() +
+                                        path.extname(file.originalname)
+                    );
+          },
 });
 //superadmin's..
 const storageBySuperAdmin = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/uploads");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname +
-                "-" +
-                req.admin.role +
-                req.admin._id +
-                "-" +
-                Date.now() +
-                path.extname(file.originalname)
-        );
-    },
+          destination: function (req, file, cb) {
+                    cb(null, './public/uploads');
+          },
+          filename: function (req, file, cb) {
+                    cb(
+                              null,
+                              file.fieldname +
+                                        '-' +
+                                        req.admin.role +
+                                        req.admin._id +
+                                        '-' +
+                                        Date.now() +
+                                        path.extname(file.originalname)
+                    );
+          },
 });
 
 const fileFilter = (req, file, callback) => {
-    const ext = path.extname(file.originalname);
-    if (ext !== ".png" && ext !== ".jpg" && ext !== ".JPG" && ext !== ".jpeg") {
-        return callback(new Error("Not Image"));
-    }
-    callback(null, true);
+          const ext = path.extname(file.originalname);
+          if (
+                    ext !== '.png' &&
+                    ext !== '.jpg' &&
+                    ext !== '.JPG' &&
+                    ext !== '.jpeg'
+          ) {
+                    return callback(new Error('Not Image'));
+          }
+          callback(null, true);
 };
 const limits = { fileSize: 2480 * 3230 };
 
@@ -69,22 +74,22 @@ const limits = { fileSize: 2480 * 3230 };
 //   { name: "citizenshipBack", maxCount: 1 },
 //   { name: "businessLicence", maxCount: 1 }
 // ]);
-exports.uploadAdminDoc = multer({ storage, fileFilter, limits }).single("doc");
+exports.uploadAdminDoc = multer({ storage, fileFilter, limits }).single('doc');
 exports.uploadAdminPhoto = multer({ storage, fileFilter, limits }).single(
-    "photo"
+          'photo'
 );
 exports.uploadUserPhoto = multer({
-    storage: storageByUser,
-    fileFilter,
-    limits,
-}).single("photo");
+          storage: storageByUser,
+          fileFilter,
+          limits,
+}).single('photo');
 
 exports.uploadProductImages = multer({ storage, fileFilter, limits }).array(
-    "productImages",
-    5
+          'productImages',
+          5
 );
 exports.uploadBannerPhoto = multer({
-    storage: storageBySuperAdmin,
-    fileFilter,
-    limits: { fileSize: 8480 * 4230 },
-}).single("bannerPhoto");
+          storage: storageBySuperAdmin,
+          fileFilter,
+          limits: { fileSize: 8480 * 4230 },
+}).single('bannerPhoto');
